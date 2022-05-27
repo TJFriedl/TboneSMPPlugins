59a1f92b4609d014acfd23f2885ee2e3be0ba013
TJFriedl <tjfriedl17@gmail.com>
2022-05-26 20:46:22 -0500

    Added Blazedtip enchant

--- src/main/java/me/tbonejdi/tboneplugins/enchants/CustomEnchants.java
+++ src/main/java/me/tbonejdi/tboneplugins/enchants/CustomEnchants.java
@@ -10,12 +10,15 @@ public class CustomEnchants {
 
     public static final Enchantment TELEPATHY = new
             EnchantmentWrapper("telepathy", "Telepathy", 1);
+    public static final Enchantment BLAZED_TIP = new EnchantmentWrapper("blazedtip", "Blazed Tip", 1);
 
     public static void register() {
         boolean registered =
-                Arrays.stream(Enchantment.values()).collect(Collectors.toList()).contains(CustomEnchants.TELEPATHY);
+                Arrays.stream(Enchantment.values()).collect(Collectors.toList()).contains(CustomEnchants.TELEPATHY)
+                && Arrays.stream(Enchantment.values()).collect(Collectors.toList()).contains(CustomEnchants.BLAZED_TIP);
         if (!registered) {
             registerEnchantment(TELEPATHY);
+            registerEnchantment(BLAZED_TIP);
         }
     }
 
--- src/main/java/me/tbonejdi/tboneplugins/enchants/EnchantEvents.java
+++ src/main/java/me/tbonejdi/tboneplugins/enchants/EnchantEvents.java
@@ -1,6 +1,7 @@
 package me.tbonejdi.tboneplugins.enchants;
 
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
@@ -15,10 +16,9 @@ public class EnchantEvents implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
-        // These are conditionals shown in the tutorial...
-        if (e.getPlayer().getInventory().getItemInMainHand() == null)
-            return;
-        if (!e.getPlayer().getInventory().getItemInMainHand().hasItemMeta())
+        // These are conditionals shown in the tutorial... Condense and clean eventually.
+        if (e.getPlayer().getInventory().getItemInMainHand() == null ||
+                !e.getPlayer().getInventory().getItemInMainHand().hasItemMeta())
             return;
         if (!e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasEnchant(CustomEnchants.TELEPATHY))
             return;
@@ -37,6 +37,16 @@ public class EnchantEvents implements Listener {
         if (drops.isEmpty()) return;
         p.getInventory().addItem(drops.iterator().next());
 
+    }
+
+    @EventHandler
+    public void onOreBreak(BlockBreakEvent e) {
+        if (e.getBlock().getType() == Material.IRON_ORE
+                && e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasEnchant(CustomEnchants.BLAZED_TIP)) {
+            e.setCancelled(true);
+            e.getBlock().getLocation().getWorld().dropItemNaturally(e.getBlock().getLocation(),
+                    new ItemStack(Material.IRON_INGOT));
+        }
     }
 
 }
